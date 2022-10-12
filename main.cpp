46cdcb382c3e2b858fa87baa4f153ed34254df1e
DaytonBere <daytonbere@gmail.com>
2022-10-11 21:33:14 -0500

    Created working user inputs

--- main.cpp
+++ main.cpp
@@ -98,11 +98,27 @@ class WebServer {
 
 int main() {
     srand(time(0));
-
     char LETTER = 'A';
-    int MAXQUEUE = 5;
-    int MAXWEBSERVERS = 7;
+    cout << "Welcome to CSCE 412 project 2 Load Balancer by Dayton Berezoski" << endl << endl;
 
+
+    int MAXWEBSERVERS ;
+    cout << "Please type the number of servers to create here (max is 26): ";
+    cin >> MAXWEBSERVERS;
+    cout << endl << endl;
+    
+    int MAXQUEUE;
+    cout << "Please type the max size of the queue (-1 to default to 2*Max Web Servers): ";
+    cin >> MAXQUEUE;
+    if (MAXQUEUE == -1) {
+        MAXQUEUE = 2 * MAXWEBSERVERS;
+    }
+    cout << endl << endl;
+
+    long long MAXTIME ;
+    cout << "Please type the number of ticks to run the server for: ";
+    cin >> MAXTIME;
+    cout << endl << endl;
 
     queue<Request*> processQueue;
     queue<Request*> awaitQueue;
@@ -118,7 +134,7 @@ int main() {
     processQueue.push(test);
 
 
-    for (int i = 0; i < 1000; i++) {
+    for (long long i = 0; i < MAXTIME; i++) {
         queue<Request*> tempQueue;
 
         while (processQueue.size() > 0) {
