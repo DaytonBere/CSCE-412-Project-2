e7c7c476d4d75c52b6a0275d2a11f7851e0baa8e
DaytonBere <daytonbere@gmail.com>
2022-10-11 21:21:54 -0500

    Fully implemented in main

--- main.cpp
+++ main.cpp
@@ -4,79 +4,129 @@
 #include <ctime>
 #include <queue>
 
+using namespace std;
+
 class Request {
     private:
 
-    std::string webServerID;
-    std::string inIP;
-    std::string outIP;
+    char webServerID;
+    string inIP;
+    string outIP;
     int timeLeft;
 
-    std::string getIP() {
+    string getIP () {
 
         int one = rand() % 256;
         int two = rand() % 256;
         int three = rand() % 256;
         int four = rand() % 256;
 
-        std::string IP = std::to_string(one) + "." + std::to_string(two) + "." + std::to_string(three) + "." + std::to_string(four);
+        string IP = to_string(one) + "." + to_string(two) + "." + to_string(three) + "." + to_string(four);
         return IP;
     }
 
     public:
 
-    Request(std::string ID) {
+    Request (char ID) {
         webServerID = ID;
         inIP = getIP();
         outIP = getIP();
         timeLeft = rand() % 100 + 1;
     }
 
-    std::string getInIP() {
+    Request () {
+        webServerID = '-';
+        inIP = getIP();
+        outIP = getIP();
+        timeLeft = 0;
+    }
+
+    string getInIP () {
         return inIP;
     }
-    std::string getOutIP() {
+    string getOutIP () {
         return outIP;
     }
-    int getTimeLeft() {
+
+    char getWebServerID () {
+        return webServerID;
+    }
+
+    int getTimeLeft () {
         return timeLeft;
     }
-    void tick() {
+
+    void tick () {
         timeLeft--;
     }
-    void printStatus() {
-        std::cout << " Web Server: " <<  webServerID <<  ", inIP: " << inIP << ", outIP: " << outIP << std::endl;
+
+    void printStatus () {
+        cout << " Web Server: " <<  webServerID <<  ", inIP: " << inIP << ", outIP: " << outIP << endl;
     }
 };
 
-// class WebServer {
-//     private:
-//     std::string webServerID;
-//     int probability;
+class WebServer {
+    private:
+    char webServerID;
+    int probability;
 
 
-//     WebServer(std::string ID) {
+    public:
+    WebServer (char ID) {
+        webServerID = ID;
+        probability = rand() % 10 + 10;
+    }
 
-//     }
+    Request* getRequest () {
+        return new Request(webServerID);
+    }
 
-// };
+    bool roll () {
+        int r = rand() % probability;
+        return r == 0;
+    }
+
+    void print () {
+        cout << "WebServerID: " << webServerID << ", Probability: " << probability << endl;
+    }
+};
+
+
+
+
+
 
 int main() {
     srand(time(0));
 
+    char LETTER = 'A';
     int MAXQUEUE = 5;
-    std::queue<Request*> processQueue;
-    std::queue<Request*> awaitQueue;
-    Request *test = new Request("D");
+    int MAXWEBSERVERS = 7;
+
+
+    queue<Request*> processQueue;
+    queue<Request*> awaitQueue;
+
+    vector<WebServer*> servers;
+    for (int i = 0; i < MAXWEBSERVERS; i++) {
+        WebServer *nWebServer = new WebServer(LETTER);
+        servers.push_back(nWebServer);
+        LETTER++;
+    }
+
+    Request *test = new Request();
     processQueue.push(test);
 
-    for (unsigned int i; i < 1000; i++){
-        std::queue<Request*> tempQueue;
+
+    for (int i = 0; i < 1000; i++) {
+        queue<Request*> tempQueue;
 
         while (processQueue.size() > 0) {
             if (processQueue.front()->getTimeLeft() <= 0) {
-                std::cout << "Time: " << i << ", ";
-                processQueue.front()->printStatus();
+                if (processQueue.front()->getWebServerID() != '-') {
+                    cout << "Time: " << i << ", ";
+                    processQueue.front()->printStatus();
+                }
                 processQueue.pop();
             } else {
                 processQueue.front()->tick();
@@ -86,10 +136,11 @@ int main() {
             }
         }
 
-        if (rand() % 10 == 0) {
-            Request *nReq = new Request("A");
-            awaitQueue.push(nReq);
-            std::cout << std::endl << "New Request " << awaitQueue.size() << std::endl << std::endl;
+        for (int j = 0; j < servers.size(); j++) {
+            if (servers[j]->roll()) {
+                Request *nReq = servers[j]->getRequest();
+                awaitQueue.push(nReq);
+            }
         }
 
         processQueue = tempQueue;
@@ -97,10 +148,10 @@ int main() {
         while (processQueue.size() < MAXQUEUE && awaitQueue.size() > 0) {
             processQueue.push(awaitQueue.front());
             awaitQueue.pop();
-            std::cout << std::endl << "Request Pushed " << processQueue.size() << std::endl << std::endl;
         }
     }
 
-    std::cout << "Ended with " << processQueue.size() << " items in the process queue" << std::endl;
-    std::cout << "Ended with " << awaitQueue.size() << " items in the await queue" << std::endl;
+    cout << endl;
+    cout << "Ended with " << processQueue.size() << " items in the process queue" << endl;
+    cout << "Ended with " << awaitQueue.size() << " items in the await queue" << endl;
 }
